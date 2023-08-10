c5eb0f0ff8dafed2660229c4d1be772fe6e2f4ec
Ayn206 <m.umerfarooq206@gmail.com>
2023-08-10 11:34:31 +0500

    Added Basic Ads, Interstital ads not working for some reason. Will add Premium tommorow, might look at rewarded adds

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.myapplication;
+import static android.content.ContentValues.TAG;
 import static com.example.myapplication.UtilityFunctions.getBitmap;
 
 import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -13,7 +15,16 @@ import android.icu.text.SimpleDateFormat;
 import android.media.Image;
 import android.media.MediaScannerConnection;
 import android.os.Bundle;
+
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Environment;
 import android.provider.ContactsContract;
@@ -32,6 +43,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import  android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
@@ -47,11 +59,17 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.Ref;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
 import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -64,15 +82,24 @@ public class MainActivity extends AppCompatActivity {
     int cout;
     //Array to Store Images
     ArrayList<Uri> mArrayUri;
-
+    int DailyUses;
+    Boolean Premium;
     //UI Elements
     Button BSelectImage;
     TextView NoSelected;
     Button CropImageButton;
+    ProgressBar progressbar;
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    InterstitialAd mInterstitialAd;
 
+    AdView mAdView;
+    AdRequest adRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         //Initialization
         setContentView(R.layout.activity_main);
@@ -80,12 +107,41 @@ public class MainActivity extends AppCompatActivity {
         BSelectImage = findViewById(R.id.BSelectImage);
         CropImageButton = findViewById(R.id.CropButton);
         NoSelected = findViewById(R.id.NoText);
+        progressbar = findViewById(R.id.progressBar);
+        pref = getSharedPreferences("com.android.app.users", Context.MODE_PRIVATE);
+        editor = pref.edit();
+        Premium = pref.getBoolean("Premium", false);
+        CheckDay();
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        adRequest = new AdRequest.Builder().build();
+        if (!Premium) {
+            mAdView = findViewById(R.id.adView);
+
+            mAdView.loadAd(adRequest);
+        }
+        InterstitAd();
+
+        if (!Premium) {
+            DailyUses = pref.getInt("DailyUses", 2);
+        }
+
+
+        progressbar.setVisibility(View.INVISIBLE);
+
+
         BSelectImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 imageChooser();
             }
         });
+
+
+
         CropImageButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -100,7 +156,7 @@ public class MainActivity extends AppCompatActivity {
     // this function is triggered when
     // the Select Image Button is clicked
     void imageChooser() {
-
+        mArrayUri.clear();
         // create an instance of the
         // intent of the type image
         Intent i = new Intent();
@@ -110,6 +166,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         startActivityForResult(Intent.createChooser(i, "Select Picture"), PICK_IMAGE_MULTIPLE);
+        CheckDay();
     }
 
 
@@ -125,17 +182,27 @@ public class MainActivity extends AppCompatActivity {
 
             try{
                 cout = data.getClipData().getItemCount();
+                if(!Premium && (cout > 100))
+                {
+                    Toast.makeText(getApplicationContext(),"100 image limit for free users exceeded",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    for (int i = 0; i < cout; i++)
+                    {
+                        Uri imageurl = data.getClipData().getItemAt(i).getUri();
+                        mArrayUri.add(imageurl);
+                    }
+                }
             }
             catch (Exception e)
             {
                 cout = 1;
-            }
-            NoSelected.setText(cout + " images selected");
-            for (int i = 0; i < cout; i++)
-            {
-                Uri imageurl = data.getClipData().getItemAt(i).getUri();
+                Uri imageurl = data.getData();
                 mArrayUri.add(imageurl);
             }
+            NoSelected.setText(cout + " images selected");
+
 
         }
     }
@@ -164,92 +231,112 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void CropImage(ArrayList ImageList, int cout) {
-        if (!Python.isStarted()) {
-            Python.start(new AndroidPlatform(this));
-
-
-        }
-        Python py = Python.getInstance();
-        PyObject pyobj = py.getModule("ScreenShotCropper");
-
-
-        for (int i = 0; i < cout; i++)
+    private void CropImage(ArrayList ImageList, int cout)
+    {
+        InterstitAd();
+        if(DailyUses != 0)
         {
+            if (!Python.isStarted()) {
+                Python.start(new AndroidPlatform(this));
 
-            Uri imageurl = (Uri) ImageList.get(i);
-            Bitmap bitmap = null;
-            try {
-                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageurl);
 
-            } catch (IOException e) {
-                throw new RuntimeException(e);
             }
-            String imgString = UtilityFunctions.getStringImage(bitmap);
-            Context context = this.getApplicationContext();
-            Resources res = context.getResources();
-            int Height = bitmap.getHeight();
-            Bitmap bp;
-            Bitmap Resized;
-            String RefString = null;
+            Python py = Python.getInstance();
+            PyObject pyobj = py.getModule("ScreenShotCropper");
+            progressbar.setVisibility(View.VISIBLE);
+            progressbar.setMax(cout);
 
-            File file = new File(imageurl.getPath());//create path from uri
-            String[] split = file.getPath().split(":");//split the path.
-            String filePath = split[0] + "/" + getFileName(imageurl);
-            PyObject obj = null;
-            Bitmap Cropped = null;
+            for (int i = 0; i < cout; i++)
+            {
 
-            switch (Height) {
-                case 1520:
-                    bp = BitmapFactory.decodeResource(context.getResources(), R.drawable.ref_1520);
-                    System.out.println(bp.getWidth());
-                    Bitmap resized = Bitmap.createScaledBitmap(bp, 43, 44, true);
-                    RefString = UtilityFunctions.getStringImage(resized);
-                    obj = pyobj.callAttr("crop", imgString, RefString, filePath);
-                    Cropped = UtilityFunctions.getBitmap(obj.toString());
-                    SaveImage(Cropped);
+                progressbar.setProgress(i);
+                Uri imageurl = (Uri) ImageList.get(i);
 
-                    break;
-                case 1920:
-                    bp = BitmapFactory.decodeResource(context.getResources(), R.drawable.ref_1080);
-                    resized = Bitmap.createScaledBitmap(bp, 82, 81, true);
-                    RefString = UtilityFunctions.getStringImage(resized);
-                    obj = pyobj.callAttr("crop", imgString, RefString, filePath);
-                    Cropped = UtilityFunctions.getBitmap(obj.toString());
-                    SaveImage(Cropped);
+                Bitmap bitmap = null;
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageurl);
 
-                    break;
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                String imgString = UtilityFunctions.getStringImage(bitmap);
+                Context context = this.getApplicationContext();
+                Resources res = context.getResources();
+                int Height = bitmap.getHeight();
+                Bitmap bp;
+                Bitmap Resized;
+                String RefString = null;
+
+                File file = new File(imageurl.getPath());//create path from uri
+                String[] split = file.getPath().split(":");//split the path.
+                String filePath = split[0] + "/" + getFileName(imageurl);
+                PyObject obj = null;
+                Bitmap Cropped = null;
+
+                switch (Height) {
+                    case 1520:
+                        bp = BitmapFactory.decodeResource(context.getResources(), R.drawable.ref_1520);
+                        System.out.println(bp.getWidth());
+                        Bitmap resized = Bitmap.createScaledBitmap(bp, 43, 44, true);
+                        RefString = UtilityFunctions.getStringImage(resized);
+                        obj = pyobj.callAttr("crop", imgString, RefString, filePath);
+                        Cropped = UtilityFunctions.getBitmap(obj.toString());
+                        SaveImage(Cropped);
+
+                        break;
+                    case 1920:
+                        bp = BitmapFactory.decodeResource(context.getResources(), R.drawable.ref_1080);
+                        resized = Bitmap.createScaledBitmap(bp, 82, 81, true);
+                        RefString = UtilityFunctions.getStringImage(resized);
+                        obj = pyobj.callAttr("crop", imgString, RefString, filePath);
+                        Cropped = UtilityFunctions.getBitmap(obj.toString());
+                        SaveImage(Cropped);
+
+                        break;
 
 
-                case 1280:
-                    bp = BitmapFactory.decodeResource(context.getResources(), R.drawable.ref_1280);
-                    resized = Bitmap.createScaledBitmap(bp, 35, 35, true);
-                    RefString = UtilityFunctions.getStringImage(resized);
-                    obj = pyobj.callAttr("crop", imgString, RefString, filePath);
-                    Cropped = UtilityFunctions.getBitmap(obj.toString());
-                    SaveImage(Cropped);
+                    case 1280:
+                        bp = BitmapFactory.decodeResource(context.getResources(), R.drawable.ref_1280);
+                        resized = Bitmap.createScaledBitmap(bp, 35, 35, true);
+                        RefString = UtilityFunctions.getStringImage(resized);
+                        obj = pyobj.callAttr("crop", imgString, RefString, filePath);
+                        Cropped = UtilityFunctions.getBitmap(obj.toString());
+                        SaveImage(Cropped);
 
-                    break;
-                case 1640:
-                    bp = BitmapFactory.decodeResource(context.getResources(), R.drawable.ref_1520);
-                    System.out.println(bp.getWidth());
-                    resized = Bitmap.createScaledBitmap(bp, 43, 44, true);
-                    RefString = UtilityFunctions.getStringImage(resized);
-                    obj = pyobj.callAttr("crop", imgString, RefString, filePath);
-                    Cropped = UtilityFunctions.getBitmap(obj.toString());
-                    SaveImage(Cropped);
+                        break;
+                    case 1640:
+                        bp = BitmapFactory.decodeResource(context.getResources(), R.drawable.ref_1520);
+                        System.out.println(bp.getWidth());
+                        resized = Bitmap.createScaledBitmap(bp, 43, 44, true);
+                        RefString = UtilityFunctions.getStringImage(resized);
+                        obj = pyobj.callAttr("crop", imgString, RefString, filePath);
+                        Cropped = UtilityFunctions.getBitmap(obj.toString());
+                        SaveImage(Cropped);
 
-                    break;
-                default:
-                    Toast.makeText(getApplicationContext(),"Skipping Over Image, Invalid Format/Unsupported Resolution",Toast.LENGTH_SHORT).show();
-                    break;
+                        break;
+                    default:
+                        Toast.makeText(getApplicationContext(),"Skipping Over Image, Invalid Format/Unsupported Resolution",Toast.LENGTH_SHORT).show();
+                        break;
+
+
+                }
+
+
+
             }
-
-
-
+            progressbar.setProgress(0);
+            NoSelected.setText("");
+            mArrayUri.clear();
+            Toast.makeText(getApplicationContext(),"Cropping Complete",Toast.LENGTH_SHORT).show();
+            progressbar.setVisibility(View.INVISIBLE);
+            DailyUses = DailyUses - 1;
+            editor.putInt("DailyUses", DailyUses);
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"Daily Limit exceeded, Wait a day or buy premium",Toast.LENGTH_SHORT).show();
         }
 
-        Toast.makeText(getApplicationContext(),"Cropping Complete",Toast.LENGTH_SHORT).show();
     }
 
     void SaveImage(Bitmap finalBitmap) {
@@ -285,6 +372,88 @@ public class MainActivity extends AppCompatActivity {
                         Log.i("ExternalStorage", "-> uri=" + uri);
                     }
                 });
+    }
+
+    private void CheckDay()
+    {
+        Calendar c = Calendar.getInstance();
+        int currentTimeSeconds = c.get(Calendar.SECOND);
+        int secondsPreviousDay = pref.getInt("seconds", 0);
+        if (secondsPreviousDay != 0){ //means there was an earlier set value from previously entering the activity
+            //compare if more than 3600*24 = 86400 (1 day in seconds) had passed
+            if (currentTimeSeconds - secondsPreviousDay > 86400){
+                editor.putInt("DailyUses", 2);
+            }
+        }
+        else {
+            pref.edit().putInt("seconds", currentTimeSeconds).apply();
+        }
+    }
+
+    private void InterstitAd()
+    {
+        InterstitialAd.load(this, "ca-app-pub-3940256099942544/3347511713", adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        // The mInterstitialAd reference will be null until
+                        // an ad is loaded.
+                        mInterstitialAd = interstitialAd;
+                        Log.i(TAG, "onAdLoaded");
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+                        Log.d(TAG, loadAdError.toString());
+                        mInterstitialAd = null;
+                    }
+                });
+
+        try {
+            mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                @Override
+                public void onAdClicked() {
+                    // Called when a click is recorded for an ad.
+                    Log.d(TAG, "Ad was clicked.");
+                }
+
+                @Override
+                public void onAdDismissedFullScreenContent() {
+                    // Called when ad is dismissed.
+                    // Set the ad reference to null so you don't show the ad a second time.
+                    Log.d(TAG, "Ad dismissed fullscreen content.");
+                    mInterstitialAd = null;
+                }
+
+                @Override
+                public void onAdFailedToShowFullScreenContent(AdError adError) {
+                    // Called when ad fails to show.
+                    Log.e(TAG, "Ad failed to show fullscreen content.");
+                    mInterstitialAd = null;
+                }
+
+                @Override
+                public void onAdImpression() {
+                    // Called when an impression is recorded for an ad.
+                    Log.d(TAG, "Ad recorded an impression.");
+                }
+
+                @Override
+                public void onAdShowedFullScreenContent() {
+                    // Called when ad is shown.
+                    Log.d(TAG, "Ad showed fullscreen content.");
+                }
+            });
+        }
+        catch (Exception e) {
+        }
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show(MainActivity.this);
+        } else {
+            Log.d("TAG", "The interstitial ad wasn't ready yet.");
+        }
+
     }
 }
 
